c15eff30b6cbbee64a7782a0fc2827823d88da3f
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-04-14 10:48:47 +0300

    first commit

---
+++ Lab9OOP2kurs/CSVRead/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSVRead;
+
+namespace CSVRead
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\НАВЧАННЯЯЯЯЯЯ\Visual Studio\Gitlab9\Lab9OOP2kurs\CSVRead\textD.csv";
+            CSV cSVReadering = new CSV();
+            cSVReadering.Reader(path);
+        }
+
+    }
+}
---
+++ Lab9OOP2kurs/JSONRead/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONRead
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
